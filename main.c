83d45b799e6ff1e231b5ee7dd137a7b499627728
zzz09800 <zzz09800@gmail.com>
2017-04-29 01:16:30 -0400

    Hotfix

--- main.c
+++ main.c
@@ -4,39 +4,29 @@
 #include <math.h>
 #include <string.h>
 
-#define MATRIX_SIZE 30
+#define MATRIX_SIZE 20
 
 //TODO: Refactor code so matrix is read from a file.
-double mat_in[MATRIX_SIZE][MATRIX_SIZE] = {{49, 0,   0,   0,   42, 0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   98,  0,   0,  0,   0,    0,   0,   0,   0,   0,   0,},
-                                           {0,  361, 342, 0,   0,  0,   0,   0,   0,   190, 0,   285, 0,  0,   0,   0,   0,   0,   0,   0,   361, 0,  0,   0,    0,   0,   0,   0,   0,   0,},
-                                           {0,  342, 373, 0,   0,  0,   0,   0,   0,   180, 0,   270, 0,  0,   0,   0,   0,   0,   133, 0,   342, 0,  0,   0,    63,  0,   0,   0,   42,  0,},
-                                           {0,  0,   0,   81,  0,  0,   0,   0,   0,   0,   0,   135, 0,  0,   108, 0,   36,  0,   0,   0,   0,   0,  0,   153,  0,   0,   0,   0,   0,   0,},
-                                           {42, 0,   0,   0,   37, 0,   18,  0,   0,   6,   0,   0,   0,  0,   0,   0,   15,  0,   0,   84,  0,   0,  0,   0,    0,   0,   6,   0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  324, 0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   18,  0,   0,   0,   0,  0,   0,    0,   0,   0,   0,   0,   0,},
-                                           {0,  0,   0,   0,   18, 0,   333, 0,   0,   108, 57,  39,  0,  0,   0,   39,  270, 0,   0,   36,  0,   0,  0,   0,    42,  0,   108, 0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  0,   0,   81,  0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   63,  0,   0,   0,  0,   0,    135, 0,   0,   126, 0,   90,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   361, 0,   0,   0,   0,  114, 0,   0,   0,   0,   0,   0,   0,   0,  0,   0,    0,   19,  0,   0,   0,   0,},
-                                           {0,  190, 180, 0,   6,  0,   108, 0,   0,   161, 40,  150, 0,  0,   0,   30,  90,  0,   0,   0,   190, 0,  0,   85,   0,   0,   36,  0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  0,   57,  0,   0,   40,  569, 247, 0,  0,   0,   295, 0,   0,   0,   228, 0,   24, 0,   136,  266, 0,   0,   0,   96,  0,},
-                                           {0,  285, 270, 135, 0,  0,   39,  0,   0,   150, 247, 875, 0,  0,   180, 169, 60,  0,   0,   156, 285, 0,  0,   271,  182, 0,   0,   0,   0,   80,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   16, 0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,    0,   0,   20,  0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   114, 0,   0,   0,   0,  232, 0,   0,   0,   0,   0,   0,   0,   0,  28,  0,    0,   146, 0,   238, 0,   84,},
-                                           {0,  0,   0,   108, 0,  0,   0,   0,   0,   0,   0,   180, 0,  0,   244, 0,   48,  0,   0,   0,   0,   0,  200, 204,  40,  0,   10,  0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  0,   39,  0,   0,   30,  295, 169, 0,  0,   0,   286, 0,   0,   0,   156, 0,   0,  0,   102,  182, 0,   0,   0,   0,   0,},
-                                           {0,  0,   0,   36,  15, 0,   270, 0,   0,   90,  0,   60,  0,  0,   48,  0,   257, 0,   0,   0,   0,   0,  0,   88,   0,   0,   90,  8,   0,   20,},
-                                           {0,  0,   0,   0,   0,  18,  0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   401, 0,   0,   0,   60, 100, 0,    0,   0,   0,   0,   0,   0,},
-                                           {0,  0,   133, 0,   0,  0,   0,   63,  0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   635, 0,   0,   0,  0,   255,  276, 0,   0,   113, 114, 70,},
-                                           {98, 0,   0,   0,   84, 0,   36,  0,   0,   0,   228, 156, 0,  0,   0,   156, 0,   0,   0,   404, 0,   0,  0,   0,    168, 0,   0,   0,   0,   0,},
-                                           {0,  361, 342, 0,   0,  0,   0,   0,   0,   190, 0,   285, 0,  0,   0,   0,   0,   0,   0,   0,   410, 0,  0,   0,    0,   0,   56,  0,   0,   0,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   0,   0,   24,  0,   0,  0,   0,   0,   0,   60,  0,   0,   0,   22, 15,  0,    0,   0,   0,   36,  16,  0,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  28,  200, 0,   0,   100, 0,   0,   0,   15, 718, 289,  80,  20,  20,  136, 0,   12,},
-                                           {0,  0,   0,   153, 0,  0,   0,   0,   0,   85,  136, 271, 0,  0,   204, 102, 88,  0,   255, 0,   0,   0,  289, 1326, 0,   0,   0,   129, 0,   30,},
-                                           {0,  0,   63,  0,   0,  0,   42,  135, 0,   0,   266, 182, 0,  0,   40,  182, 0,   0,   276, 168, 0,   0,  80,  0,    687, 0,   4,   210, 54,  150,},
-                                           {0,  0,   0,   0,   0,  0,   0,   0,   19,  0,   0,   0,   0,  146, 0,   0,   0,   0,   0,   0,   0,   0,  20,  0,    0,   462, 0,   170, 0,   60,},
-                                           {0,  0,   0,   0,   6,  0,   108, 0,   0,   36,  0,   0,   20, 0,   10,  0,   90,  0,   0,   0,   56,  0,  20,  0,    4,   0,   487, 0,   0,   266,},
-                                           {0,  0,   0,   0,   0,  0,   0,   126, 0,   0,   0,   0,   0,  238, 0,   0,   8,   0,   113, 0,   0,   36, 136, 129,  210, 170, 0,   926, 0,   252,},
-                                           {0,  0,   42,  0,   0,  0,   0,   0,   0,   0,   96,  0,   0,  0,   0,   0,   0,   0,   114, 0,   0,   16, 0,   0,    54,  0,   0,   0,   356, 0,},
-                                           {0,  0,   0,   0,   0,  0,   0,   90,  0,   0,   0,   80,  0,  84,  0,   0,   20,  0,   70,  0,   0,   0,  12,  30,   150, 60,  266, 252, 0,   463,}};
+double mat_in[MATRIX_SIZE][MATRIX_SIZE] = {{   9,   18,    0,   45,   39,   45,   18,   36,   27,    3,    6,   21,   30,   57,    9,   18,    0,   36,   48,   33 },
+                                           {  18,  100,   56,  162,   94,  170,   52,   96,  110,  126,   12,   58,   76,  258,   90,   92,  104,  200,  184,   82 },
+                                           {   0,   56,  130,  180,   14,   70,   50,  174,   49,  141,  135,  104,  131,  180,  162,  193,  208,  130,   77,  158 },
+                                           {  45,  162,  180,  476,  218,  320,  164,  435,  214,  207,  231,  262,  350,  542,  273,  376,  288,  355,  353,  398 },
+                                           {  39,   94,   14,  218,  394,  296,  102,  337,  309,  180,   56,  392,  311,  424,  119,  321,  232,  213,  468,  406 },
+                                           {  45,  170,   70,  320,  296,  430,  282,  381,  361,  270,  172,  394,  387,  646,  279,  423,  252,  373,  604,  284 },
+                                           {  18,   52,   50,  164,  102,  282,  722,  606,  246,  458,  379,  585,  470,  517,  515,  584,  542,  304,  684,  451 },
+                                           {  36,   96,  174,  435,  337,  381,  606, 1168,  570,  521,  756,  956,  829,  842,  856, 1133,  947,  483,  864,  925 },
+                                           {  27,  110,   49,  214,  309,  361,  246,  570,  601,  354,  375,  560,  568,  770,  462,  724,  406,  402,  710,  427 },
+                                           {   3,  126,  141,  207,  180,  270,  458,  521,  354, 1210,  418,  725,  588, 1075,  716,  674,  893,  646, 1007,  983 },
+                                           {   6,   12,  135,  231,   56,  172,  379,  756,  375,  418,  848,  801,  744,  698,  801, 1023,  730,  278,  639,  614 },
+                                           {  21,   58,  104,  262,  392,  394,  585,  956,  560,  725,  801, 1357, 1011, 1050,  900, 1265, 1057,  473, 1278, 1136 },
+                                           {  30,   76,  131,  350,  311,  387,  470,  829,  568,  588,  744, 1011, 1352, 1190, 1056, 1390,  850,  912, 1193,  937 },
+                                           {  57,  258,  180,  542,  424,  646,  517,  842,  770, 1075,  698, 1050, 1190, 2192, 1174, 1642, 1229, 1342, 1753, 1245 },
+                                           {   9,   90,  162,  273,  119,  279,  515,  856,  462,  716,  801,  900, 1056, 1174, 1250, 1480, 1252, 1013, 1237,  935 },
+                                           {  18,   92,  193,  376,  321,  423,  584, 1133,  724,  674, 1023, 1265, 1390, 1642, 1480, 2223, 1646, 1237, 1779, 1207 },
+                                           {   0,  104,  208,  288,  232,  252,  542,  947,  406,  893,  730, 1057,  850, 1229, 1252, 1646, 1822, 1138, 1524, 1344 },
+                                           {  36,  200,  130,  355,  213,  373,  304,  483,  402,  646,  278,  473,  912, 1342, 1013, 1237, 1138, 2045, 1922, 1262 },
+                                           {  48,  184,   77,  353,  468,  604,  684,  864,  710, 1007,  639, 1278, 1193, 1753, 1237, 1779, 1524, 1922, 3227, 1979 },
+                                           {  33,   82,  158,  398,  406,  284,  451,  925,  427,  983,  614, 1136,  937, 1245,  935, 1207, 1344, 1262, 1979, 2261 }};
 
 
 struct node_info {
@@ -124,15 +114,15 @@ int main(int argc, char *argv[]) {
 	float recv[MATRIX_SIZE][MATRIX_SIZE] = {0};
 	int available[MATRIX_SIZE] = {0};
 
-	/******************* construct iteration_per_rank *******************/
-	int iteration = MATRIX_SIZE / num_proc;
+	//==================== construct iteration_per_rank ====================//
+	int total_iteration = MATRIX_SIZE / num_proc;
 	if (rank_id < MATRIX_SIZE % num_proc) {
-		iteration++;
+		total_iteration++;
 	}
-	//printf("%d proc has %d iterations\n",rank_id, iteration);
-	self_send_map=malloc(sizeof(struct send_map)*iteration);
-	self_recv_map=malloc(sizeof(int*)*iteration);
-	for(i=0;i<iteration;i++)
+	//printf("%d proc has %d iterations\n",rank_id, total_iteration);
+	self_send_map=malloc(sizeof(struct send_map)*total_iteration);
+	self_recv_map=malloc(sizeof(int*)*total_iteration);
+	for(i=0;i<total_iteration;i++)
 	{
 		self_recv_map[i]=malloc(sizeof(int)*MATRIX_SIZE);
 	}
@@ -140,16 +130,16 @@ int main(int argc, char *argv[]) {
 	if (rank_id == 0) {
 		iteration_per_rank = (int *) malloc(sizeof(int) * num_proc);
 
-		iteration_per_rank[0] = iteration;
+		iteration_per_rank[0] = total_iteration;
 		for (i = 1; i < num_proc; i++) {
 			MPI_Recv(&iteration_per_rank[i], 1, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
 		}
 
 	} else {
-		MPI_Send(&iteration, 1, MPI_INT, 0, rank_id, MPI_COMM_WORLD);
+		MPI_Send(&total_iteration, 1, MPI_INT, 0, rank_id, MPI_COMM_WORLD);
 	}
 
-	/******************* construct rank_col_map *******************/
+	//==================== construct rank_col_map ====================//
 	int temp;
 	int *self_cols;
 	if (rank_id == 0) {
@@ -157,7 +147,7 @@ int main(int argc, char *argv[]) {
 		for (temp = 0; temp < num_proc; temp++) {
 			rank_col_map[temp] = (int *) malloc(sizeof(int) * iteration_per_rank[temp]);
 		}
-		self_cols = (int *) malloc(sizeof(int) * iteration);
+		self_cols = (int *) malloc(sizeof(int) * total_iteration);
 		j = 0;
 		for (i = 0; i < MATRIX_SIZE; i++) {
 			if (i % num_proc == 0) {
@@ -166,12 +156,12 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		memcpy(rank_col_map[0], self_cols, sizeof(int) * iteration);
+		memcpy(rank_col_map[0], self_cols, sizeof(int) * total_iteration);
 		for (temp = 1; temp < num_proc; temp++) {
 			MPI_Irecv(rank_col_map[temp], iteration_per_rank[temp], MPI_INT, temp, temp, MPI_COMM_WORLD, &request);
 		}
 	} else {
-		self_cols = (int *) malloc(sizeof(int) * iteration);
+		self_cols = (int *) malloc(sizeof(int) * total_iteration);
 		j = 0;
 		for (i = 0; i < MATRIX_SIZE; i++) {
 			if (i % num_proc == rank_id) {
@@ -179,7 +169,7 @@ int main(int argc, char *argv[]) {
 				j++;
 			}
 		}
-		MPI_Isend(self_cols, iteration, MPI_INT, 0, rank_id, MPI_COMM_WORLD, &request);
+		MPI_Isend(self_cols, total_iteration, MPI_INT, 0, rank_id, MPI_COMM_WORLD, &request);
 	}
 	MPI_Wait(&request, MPI_STATUSES_IGNORE);
 
@@ -194,16 +184,24 @@ int main(int argc, char *argv[]) {
         }
     }*/
 
-	/******************* computation *******************/
+	//==================== computation ====================//
 	int iter;
 	int recv_col;
 	int send_count;
 	int send_rank[MATRIX_SIZE];
 
-	//Niconiconi!
-	//Niconiconi!
-	//Niconiconi!
-	for (iter = 0; iter < iteration; iter++) {
+	/*if(rank_id==3){
+		for (iter = 0; iter < total_iteration; iter++) {
+			current_node = &all_columns_orig[self_cols[iter]];
+			printf("Col %d: ",current_node->col_no);
+			for(i=0;i<current_node->dependency_count;i++)
+				printf("%d ",current_node->dependency_col[i]);
+			printf("\n");
+		}
+	}*/
+
+
+	for (iter = 0; iter < total_iteration; iter++) {
 		k = 0;
 		current_node = &all_columns_orig[self_cols[iter]];
 		for (i = current_node->col_no; i < MATRIX_SIZE; i++) {
@@ -222,10 +220,36 @@ int main(int argc, char *argv[]) {
 		}
 		send_count = k;
 		self_send_map[iter].target_count=send_count;
+
+		//Unlimited Debug Works
+		for (i=0; i<self_send_map[iter].target_count; i++) {
+			j=i+1;
+			while(j<self_send_map[iter].target_count) {
+				if (self_send_map[iter].target_procs[j] == self_send_map[iter].target_procs[i]) {
+					for (k=j; k<self_send_map[iter].target_count; k++) {
+						self_send_map[iter].target_procs[k] = self_send_map[iter].target_procs[k + 1];
+					}
+					self_send_map[iter].target_count--;
+				} else
+					j++;
+			}
+		}
+
+	}
+
+	for(iter=0;iter<total_iteration; iter++)
+	{
+		current_node = &all_columns_orig[self_cols[iter]];
+		printf("Col %d going to: ",current_node->col_no);
+		for(j=0;j<self_send_map[iter].target_count;j++)
+		{
+			printf("%d ",self_send_map[iter].target_procs[j]);
+		}
+		printf("\n");
 	}
 
 
-	for (iter = 0; iter < iteration; iter++) {
+	for (iter = 0; iter < total_iteration; iter++) {
 		k=0;
 		current_node = &all_columns_orig[self_cols[iter]];
 		for (i = 0; i < current_node->dependency_count; i++) {
@@ -239,20 +263,18 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	//Niconiconi!
-	//Niconiconi!
-	//Niconiconi!
 
+	int sssss=0;
 
-	for (iter = 0; iter < iteration; iter++) {
+	for (iter = 0; iter < total_iteration; iter++) {
 		current_node = &all_columns_orig[self_cols[iter]];
-		//printf("rank %d compute %d col at iteration %d\n",rank_id,current_node->col_no,iter);
+		//printf("rank %d compute %d col at total_iteration %d\n",rank_id,current_node->col_no,iter);
 		if (current_node->tier_level == 0) {
 			cdiv(mat_in, current_node->col_no);
 			available[current_node->col_no] = 1;
 
 		} else {
-			/*******  receive and cmod  ********/
+			//==========  receive and cmod  ==========//
 			for (i = 0; i < current_node->dependency_count; i++) {
 				recv_col = current_node->dependency_col[i];
 				int recv_rank;
@@ -264,9 +286,12 @@ int main(int argc, char *argv[]) {
 					}
 				}*/
 
-				if (rank_id == recv_rank) {
-					available[current_node->col_no] = 1;
+				if (rank_id == recv_rank && available[recv_col]==1) {
+					//available[current_node->col_no] = 1;
 					cmod(mat_in, current_node->col_no, current_node->dependency_col[i]);
+					if(current_node->col_no==5){
+						printf("Cmoding %d with %d\n",current_node->col_no,current_node->dependency_col[i]);
+					}
 				} else if (available[recv_col] == 0) {
 					//printf("rank %d computing %d want to recv col %d from rank %d\n", rank_id, current_node->col_no, recv_col, recv_rank);
 					MPI_Irecv(buffer_mat[recv_col],
@@ -285,15 +310,23 @@ int main(int argc, char *argv[]) {
 					}
 
 					cmod(mat_in, current_node->col_no, current_node->dependency_col[i]);
+					if(current_node->col_no==5){
+						printf("Cmoding %d with %d\n",current_node->col_no,current_node->dependency_col[i]);
+					}
 				} else {
 					//printf("rank %d computing %d start without communication\n",rank_id,current_node->col_no);
 					cmod(mat_in, current_node->col_no, current_node->dependency_col[i]);
+					if(current_node->col_no==5){
+						printf("Cmoding %d with %d\n",current_node->col_no,current_node->dependency_col[i]);
+					}
 				}
 			}
+
 			cdiv(mat_in, current_node->col_no);
+			available[current_node->col_no] = 1;
 		}
 
-		/*******  send  ********/
+		//==========  send  ==========//
 		//calculate which rank to send
 		/*k = 0;
 		//dependent col must appear after current col
@@ -331,16 +364,16 @@ int main(int argc, char *argv[]) {
 			MPI_Isend(buffer_mat[current_node->col_no],
 			          MATRIX_SIZE,
 			          MPI_DOUBLE,
-			          //send_rank[i],
-			          self_send_map[iter].target_procs[i],
-			          current_node->col_no,
-			          MPI_COMM_WORLD, &request);
+					//send_rank[i],
+					  self_send_map[iter].target_procs[i],
+					  current_node->col_no,
+					  MPI_COMM_WORLD, &request);
 		}
 
 
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
-	if (iteration * num_proc < MATRIX_SIZE) {
+	if (total_iteration * num_proc < MATRIX_SIZE) {
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
@@ -357,7 +390,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	} else {
-		for (i = 0; i < iteration; i++) {
+		for (i = 0; i < total_iteration; i++) {
 			for (j = 0; j < MATRIX_SIZE; j++) {
 				buffer_mat[self_cols[i]][j] = mat_in[j][self_cols[i]];
 			}
@@ -367,20 +400,20 @@ int main(int argc, char *argv[]) {
 	MPI_Wait(&request, MPI_STATUSES_IGNORE);
 
 	// test final result
-    if(rank_id==0)
-    {
-        for(i=0;i<MATRIX_SIZE;i++)
-        {
-            for(j=0;j<MATRIX_SIZE;j++)
-            {
-	            if(i>=j)
-                    printf("%4.0lf",mat_in[i][j]);
-	            else
-		            printf("%4.0lf",0.00);
-            }
-            printf("\n");
-        }
-    }
+	if(rank_id==0)
+	{
+		for(i=0;i<MATRIX_SIZE;i++)
+		{
+			for(j=0;j<MATRIX_SIZE;j++)
+			{
+				if(i>=j)
+					printf("%4.0lf",mat_in[i][j]);
+				else
+					printf("%4.0lf",0.00);
+			}
+			printf("\n");
+		}
+	}
 
 	MPI_Finalize();
 }
